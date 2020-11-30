588af53bb843d9a386a7631a5f034aa2c3672346
Alexander Landberg <alexander.landberg@isobar.com>
2020-11-30 17:53:45 +0100

    test2

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
           <a href="/footer">Footer</a>
         </li>
         <li>
-          <a href="/news_Dynamic">News Feed</a>
+          <a href="/newsFeed">News Feed</a>
         </li>
 
       </ul>
